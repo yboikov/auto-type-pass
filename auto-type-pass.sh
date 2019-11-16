3da9a6c7548da80ff055f4fb658eece659c17b78
Yordan Boikov <y.boikov@gmail.com>
2019-11-16 14:14:53 +0200

    Ignore case in grep

--- auto-type-pass.sh
+++ auto-type-pass.sh
@@ -10,7 +10,7 @@ find ~/.password-store/ -name "*.gpg" -print0 |
 	while IFS= read -r -d '' line; 
 	do 
 		kname=`basename ${line%.gpg}`; 
-		if grep -q ${kname} <<<${name};
+		if grep -iq ${kname} <<<${name};
 		then 
 			PASSWORD_STORE_CLIP_TIME=1 pass -c ${kname} 
 			xdotool key --delay 50 --clearmodifiers "ctrl+v" "Return"
