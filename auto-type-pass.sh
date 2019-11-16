feab19a3d80e65169e3873a39f1d1d440955b55b
Yordan Boikov <y.boikov@gmail.com>
2019-11-16 13:42:26 +0200

    first commit

---
+++ auto-type-pass.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+win_id=$(xdotool getactivewindow)
+name=$(xdotool getwindowname ${win_id})
+name=${name#*@}
+name=${name%:*}
+find ~/.password-store/ -name "*.gpg" -print0 | 
+	while IFS= read -r -d '' line; 
+	do 
+		kname=`basename ${line%.gpg}`; 
+		if grep -q ${kname} <<<${name};
+		then 
+			PASSWORD_STORE_CLIP_TIME=1 pass -c ${kname} 
+			xdotool key --delay 50 --clearmodifiers "ctrl+v" "Return"
+		fi; 
+	done
